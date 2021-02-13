c32cf4f2e1f0dd50150aa102df912f56359c4dcc
Elías De Vargas <sailedvc@gmail.com>
2021-02-13 16:06:47 -0500

    [FEAT] add Typescript to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      Hi for Home page
-    </div>
-  )
+    return (
+        <div>
+            <Navbar/>
+            Hi for Home page
+        </div>
+    )
 }
 
 export default Home

eb2adfd137684da0a17592dbd9379fb5bdbb60e3
Elías De Vargas <sailedvc@gmail.com>
2021-02-13 18:33:23 -0500

    [FEAT] Fetching people on home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
+    const [people, setPeople] = useState<TPerson[]>([])
+
+    useEffect(() => {
+    getData()
+    }, [])
+
+    const getData = async () => {
+        window.fetch('/api/people')
+            .then( response => response.json())
+            .then(json => setPeople(json))
+    }
+
     return (
         <div>
             <Navbar/>
-            Hi for Home page
+            {people.map(person => (
+                <div key={person.id}>{person.name}</div>)
+            )}
         </div>
     )
 }

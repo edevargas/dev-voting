c5e560405dc5f2844033d73ce88d1e4c294168d3
Elías De Vargas <sailedvc@gmail.com>
2021-02-13 09:45:53 -0500

    1. Init project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      Hi for Home page
+    </div>
+  )
+}
+
+export default Home
---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Hi from login page
+    </div>
+  )
+}
+
+export default Login
